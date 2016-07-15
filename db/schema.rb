c40518e62f899ec27b272781fdf08bf748a36a4d
Ganaraja <ganaraja@gmail.com>
2016-07-15 23:19:54 +0530

    Checkpoint 4 - Routing Error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715164345) do
+ActiveRecord::Schema.define(version: 20160715170845) do
 
   create_table "items", force: :cascade do |t|
     t.integer  "list_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160715164345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "password"
+    t.string   "username"
   end
 
 end
